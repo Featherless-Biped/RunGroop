42b1358d52f887fa9890b9c26e4efb7201fd4e4d
Featherless-Biped <114402536+Featherless-Biped@users.noreply.github.com>
2023-08-29 11:00:18 +0300

    Unit Testing exercise
    This will be an exercise for writing unit tests for an MVC app. I am starting with testing the controllers

---
+++ RunGroopWebApp.Tests/Controllers/ClubControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using RunGroopWebApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunGroopWebApp.Tests.Controllers
+{
+    public class ClubControllerTests
+    {
+        private IClubRepository _clubRepository;
+
+        public ClubControllerTests() 
+        {
+            // Dependency
+            _clubRepository = A.Fake<IClubRepository>();
+        }    
+    }
+}
