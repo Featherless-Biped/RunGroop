4cbebfb13e9462b0157671bc29965cc8cdc8a53f
Featherless-Biped <114402536+Featherless-Biped@users.noreply.github.com>
2023-08-29 16:36:52 +0300

    Tests for the repo

--- RunGroopWebApp.Tests/Controllers/ClubControllerTests.cs
+++ RunGroopWebApp.Tests/Controllers/ClubControllerTests.cs
@@ -1,21 +1,118 @@
 ﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Controllers;
+using RunGroopWebApp.Data.Enum;
+using RunGroopWebApp.Helpers;
 using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace RunGroopWebApp.Tests.Controllers
 {
     public class ClubControllerTests
     {
+        private ClubController _clubController;
         private IClubRepository _clubRepository;
+        private IPhotoService _photoService;
+        private IHttpContextAccessor _httpContextAccessor;
 
-        public ClubControllerTests() 
+
+        private List<Club> fakeClubs;
+        private Club fakeClub;
+        private Address address1;
+        private Address address2;
+
+        public ClubControllerTests()
         {
             // Dependency
             _clubRepository = A.Fake<IClubRepository>();
-        }    
+            _photoService = A.Fake<IPhotoService>();
+            _httpContextAccessor = A.Fake<IHttpContextAccessor>();
+
+            // SUT
+            _clubController = new ClubController(_clubRepository, _photoService);
+
+            // Addresses
+            address1 = new Address
+            {
+                Id = 1, 
+                Street = "15 Yemen Road",
+                City = "Sanaa",
+                State = "Yemen", 
+                ZipCode = 1819101
+            };
+            address2 = new Address
+            {
+                Id = 1,
+                Street = "15 Yemen Road",
+                City = "Yemen",
+                State = "NewYork",
+                ZipCode = 1812666
+            };
+
+            // Fake clubs
+            fakeClubs = new List<Club>
+            {
+                new Club
+                {
+                    Id = 1,
+                    Title = "Club1",
+                    Description = "Description1",
+                    Image = "Image1.jpg",
+                    AddressId = 1,
+                    Address = address1,
+                    ClubCategory = ClubCategory.Trail,
+                    AppUserId = "User1"
+                },
+                new Club
+                {
+                    Id = 2,
+                    Title = "Club2",
+                    Description = "Description2",
+                    Image = "Image2.jpg",
+                    AddressId = 2,
+                    Address = address2,
+                    ClubCategory = ClubCategory.Endurance,
+                    AppUserId = "User2"
+                }
+            };
+
+            fakeClub = fakeClubs[0];
+        }
+
+        [Fact]
+        public void ClubController_Index_ReturnsSuccess()
+        {
+            // Arrange
+            A.CallTo(() => _clubRepository.GetAll()).Returns(fakeClubs);
+
+            // Act
+            var result = _clubController.Index();
+
+            // Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void ClubController_Detail_ReturnsSuccess()
+        {
+            // Arrange
+            var id = 1;
+            A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(fakeClub);
+
+            // Act
+            var result = _clubController.DetailClub(id, "RunningClub");
+
+            // Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
     }
 }
---
+++ RunGroopWebApp.Tests/Repository/ClubRepositoryTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Data.Enum;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RunGroopWebApp.Tests.Repository
+{
+    public class ClubRepositoryTests
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Clubs.CountAsync() <= 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    databaseContext.Clubs.Add(
+                        new Club()
+                        {
+                            Title = "Club1",
+                            Description = "Description1",
+                            Image = "Image1.jpg",
+                            Address = new Address
+                            {
+                                Street = "15 Yemen Road",
+                                City = "Yemen",
+                                State = "Yemen",
+                                ZipCode = 1819101
+                            },
+                            ClubCategory = ClubCategory.Trail,
+                            AppUserId = "User1"
+                        });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+        }
+        [Fact]
+        public async void ClubRepository_Add_ReturnsBool()
+        {
+            //Arrange
+            var club = new Club()
+            {
+                Title = "Club1",
+                Description = "Description1",
+                Image = "Image1.jpg",
+                Address = new Address
+                {
+                    Street = "15 Yemen Road",
+                    City = "Yemen",
+                    State = "Yemen",
+                    ZipCode = 1819101
+                },
+                ClubCategory = ClubCategory.Trail,
+                AppUserId = "User1"
+            };
+            var dbContext = await GetDbContext();
+            //dbContext.Clubs.AsNoTracking();
+            var clubRepository = new ClubRepository(dbContext);
+            
+            //Act
+            var result = clubRepository.Add(club);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+        [Fact]
+        public async void ClubRepository_GetByIdAsync_ReturnsBool()
+        {
+            //Arrange
+            var id = 1;
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            var result = clubRepository.GetByIdAsync(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<Club>>();
+        }
+    }
+}
